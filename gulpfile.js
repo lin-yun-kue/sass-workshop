9f8925afdfa453363624f12099294e8e4fa07358
Kyle_Lin <b40885@gmail.com>
2021-07-20 19:16:24 +0800

    update gulp and add import sample

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const change = require('gulp-changed');
 const watch = require('gulp-watch');
 
-gulp.task("build-web", () => {
+function build() {
     return gulp.src("src/**/*.scss")
-        .pipe(sass())
+        .pipe(sass().on('error', e => console.log(e)))
+        // .pipe(sass({outputStyle: 'compressed'}).on('error', e => console.log(e)))
         .pipe(gulp.dest("build"))
-});
+}
 
-gulp.task("launch-web", () => {
-    watch(["src/**/*.scss"])
-        .on("change", e => console.log("Scss file changed: " + e))
-        .pipe(sass())
-        .pipe(gulp.dest("build"))
-});
+function launch() {
+    watch(["src/**/*.scss"], () => {
+        build();
+    })
+    .on("change", e => console.log("Scss file changed: " + e))
+}
+
+exports.build = build;
+exports.launch = launch;
