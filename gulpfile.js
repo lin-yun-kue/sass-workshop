32382fc7edba6f36966c1e7c07e78d53106fb61b
Kyle_LIN <kyle.lin@xuenn.com>
2021-11-11 17:04:53 +0800

    adjust gulp file and move index into src

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,24 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const watch = require('gulp-watch');
 
-function build() {
-    return gulp.src("src/**/*.scss")
+async function build() {
+    compileScss();
+    moveFile();
+}
+
+function compileScss() {
+    gulp.src("src/**/*.scss")
         .pipe(sass().on('error', e => console.log(e)))
         // .pipe(sass({outputStyle: 'compressed'}).on('error', e => console.log(e)))
         .pipe(gulp.dest("build"))
+}
+
+function moveFile() {
+    const imgDest = "build/img";
+    gulp.src("src/img/**/*").pipe(gulp.dest(imgDest));
+
+    const dest = "build";
+    gulp.src("src/**/*.html").pipe(gulp.dest(dest));
 }
 
 function launch() {
