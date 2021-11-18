09e6bdd9f270f2cc00cf8ebb162d1d1e060b36dc
Kyle_LIN <kyle.lin@xuenn.com>
2021-11-18 19:58:57 +0800

    subscribe temp commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-    const flkty = new Flickity( '.js-carousel', {
+    const bannerFlkty = new Flickity('.js-banner-carousel', {
         autoPlay: 3000,
         prevNextButtons: false,
         wrapAround: true,
         pauseAutoPlayOnHover: true,
       });
+    const enjoyFlkty = new Flickity('.js-enjoy-carousel', {
+        autoPlay: 2000,
+        prevNextButtons: false,
+        wrapAround: true,
+        pauseAutoPlayOnHover: true,
+        pageDots: false,
+        contain: true
+    });
   });
